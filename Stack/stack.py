31ba3060f7d044b5e4667d45893fd75480bdf074
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-07-29 18:59:20 +0545

    Add files via upload

---
+++ Stack/stack.py
@@ -0,0 +1,28 @@
+stack=[]
+def push():
+    element=int(input("Enter the number:\n"))
+    stack.append(element)
+    print(stack)
+
+def pop_element():
+    if not stack:
+        print("The stack is empty")
+    else:
+        e=stack.pop()
+        print(f"The deleted item is {e}")
+        print(stack)
+while True:
+    print("1.Push\n2.Pop\n3.Quit\n")
+    choice=int(input())
+    if choice==1:
+        push()
+    elif choice==2:
+        pop_element()
+    elif choice==3:
+        break
+    else:
+        print("You entered the wrong choice.")
+
+
+
+
