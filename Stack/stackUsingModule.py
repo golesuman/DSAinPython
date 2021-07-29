15c4cd172909cd50ece0d3a90d8c3c1816c31c96
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-07-29 19:05:08 +0545

    Add files via upload

---
+++ Stack/stackUsingModule.py
@@ -0,0 +1,12 @@
+import queue
+stack=queue.LifoQueue(5)
+stack.put(3)
+stack.put(5)
+stack.put(8)
+stack.put(9)
+stack.put(10)
+print(stack.get())
+print(stack.get())
+print(stack.get())
+print(stack.get())
+print(stack.get())
