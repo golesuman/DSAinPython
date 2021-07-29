f351f2e65d83fdc9d8da8dfae642aa34e093bb50
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-07-29 19:10:34 +0545

    Add files via upload

---
+++ Stack/collectionsstack.py
@@ -0,0 +1,18 @@
+import collections
+stack=collections.deque()
+print(stack)
+stack.append(5)
+print(stack)
+stack.append(3)
+stack.append(6)
+stack.append(9)
+
+stack.append(4)
+stack.append(8)
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
