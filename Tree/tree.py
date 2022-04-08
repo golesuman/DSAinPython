d30886977eb40cd7c0825c50ef66151fd6dd30e8
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-08 20:39:47 +0545

    tree

---
+++ Tree/tree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,data= None) -> None:
+        self.data = data
+        self.right = None
+        self.left = None
+
+class BinaryTree:
+    def __init__(self) -> None:
+        self.root = None
+        
+
+ 
+        
+
+
+newTree = BinaryTree()
+
