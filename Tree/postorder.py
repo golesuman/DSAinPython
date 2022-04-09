fdb988990c72a2304f06ab62244bf596aa877756
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-09 08:55:29 +0545

    postordertraversal

---
+++ Tree/postorder.py
@@ -0,0 +1,25 @@
+# from tkinter import N
+
+
+class Node:
+    def __init__(self,data) -> None:
+        self.data = data
+        self.right = None
+        self.left = None
+    
+
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data)
+
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+print("postorder traversal is:")
+postorder(root)
