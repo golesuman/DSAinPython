7cf48fc3f720e85ae03d35706a457123ebed9a24
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-13 18:48:12 +0545

    Finding Duplicates in an array

---
+++ Arrays/findingduplicates.py
@@ -0,0 +1,12 @@
+def findDuplicate(array,size):
+    array.sort()
+    for i in range(size):
+        if array[i] == array[i+1]:
+            return array[i]
+
+
+if __name__== "__main__":
+    array=[1,3,5,3,2]
+    size = len(array)
+    result = findDuplicate(array, size)
+    print(result)
