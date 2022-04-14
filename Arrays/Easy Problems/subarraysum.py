53c8dcd0afcf7a328759ab1160446b4c5b61fd37
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-14 14:43:07 +0545

    Subarray sum equal to key

---
+++ Arrays/Easy Problems/subarraysum.py
@@ -0,0 +1,16 @@
+def SubarraySum(arr, n, key):
+    for i in range(n):
+        currentSum = arr[i]
+        j = i + 1
+        while j <= n:
+            if currentSum == key:
+                print(f'The indices are {i} and {j-1} ')
+
+            if currentSum > key or j == n:
+                 break
+            currentSum += arr[j]
+            j += 1
+            
+arr = [1,2,7,3,5]
+key = 8
+SubarraySum(arr,len(arr),key)
