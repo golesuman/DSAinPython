5255d896250d7ea7ba3d23faae2ec75bae399548
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-15 07:11:04 +0545

    reverse an array using iterative method

---
+++ Arrays/Easy Problems/reversingarrayiterative.py
@@ -0,0 +1,16 @@
+def reverseArray(arr,n):
+    start = 0
+    end = n-1
+    while start <= end :
+        temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start += 1
+        end -= 1
+    
+    return arr
+
+
+arr = [1,2,3,4,5]
+result = reverseArray(arr,len(arr))
+print(result)
