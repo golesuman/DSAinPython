407d4aa0a2b3ad3db196f2d87a2a7b5d0ab33c31
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-15 07:06:50 +0545

    reversing an array using recursion

---
+++ Arrays/Easy Problems/reversingarray.py
@@ -0,0 +1,10 @@
+def reverseArray(A,start,end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseArray(A, start+1, end-1)
+
+
+A = [1,2,3,4,5]
+reverseArray(A,0,len(A)-1)
+print(A)
