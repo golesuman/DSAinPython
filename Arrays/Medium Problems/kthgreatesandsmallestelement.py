5fdffc3f1ecb0ad43a544823588702c3786c8d2e
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-15 07:38:55 +0545

    kth largest element

---
+++ Arrays/Medium Problems/kthgreatesandsmallestelement.py
@@ -0,0 +1,11 @@
+def kthGreatestElement(arr, n, k):
+    arr.sort()
+    last = n-1
+    return arr[last - k + 1]
+
+
+arr=[1,2,10,3,55,23,103]
+n = len(arr)
+k = 1
+result = kthGreatestElement(arr, n, k)
+print(result)
