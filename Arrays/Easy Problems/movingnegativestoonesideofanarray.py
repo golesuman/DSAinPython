1318dc122fedebdd688188c3fef942d8e9910fbe
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-16 12:08:58 +0545

    moving negatives to the one end of an array

---
+++ Arrays/Easy Problems/movingnegativestoonesideofanarray.py
@@ -0,0 +1,24 @@
+from curses import newpad
+from xml.etree.ElementTree import Element
+
+
+def MoveNegatives(arr,n):
+    j = 0 
+    for i in range(n):
+        if arr[i] < 0:
+            temp = arr [i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            j += 1
+    return arr
+
+
+
+a = [-1, 3, 5, -12,10]
+n = len(a)
+result = MoveNegatives(a,n)
+print(result)
+
+
+
+
