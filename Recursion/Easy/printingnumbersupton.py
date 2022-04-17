0802b03a9c550837dfce5844229e62451cc5b2e6
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-17 08:37:29 +0545

    printing N numbers
    Printing numbers in ascending and descending order

---
+++ Recursion/Easy/printingnumbersupton.py
@@ -0,0 +1,17 @@
+def PrintingNumbersAscending(n):
+    if n == 0:
+        return 0 
+
+    PrintingNumbersAscending(n-1)
+    print(n)
+
+
+def PrintingNumberDescending(n):
+    if n == 0:
+        return 0 
+    print(n)
+    PrintingNumberDescending(n-1)
+if __name__ == '__main__':
+    PrintingNumbersAscending(5)
+    print("Descending")
+    PrintingNumberDescending(5)
