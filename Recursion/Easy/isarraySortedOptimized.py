c2515ba197d3861eab20e6d3590a0250b34fa7f4
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-17 19:14:21 +0545

    is array sorted better version
    in this version we don't have to copy the whole array again and again

---
+++ Recursion/Easy/isarraySortedOptimized.py
@@ -0,0 +1,15 @@
+
+
+def isArraySorted(arr,start):
+    n = len(arr)
+    if start == n-1 or start == n:
+        return True
+    if arr[start] > arr[start+1]:
+        return False
+    
+    return isArraySorted(arr,start+ 1)
+
+
+if __name__ == '__main__':
+    arr = [1,2,3,40,5]
+    print(isArraySorted(arr,start = 0))
