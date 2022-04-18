0a65ae10cfbe36ad63d4043103ab3727ee473c7a
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-18 19:56:25 +0545

    remove given char in the string

---
+++ Recursion/Easy/removegivenelementfromstring.py
@@ -0,0 +1,13 @@
+def removeGivenElement(arr, x):
+    if len(arr) == 0:
+        return arr
+        
+
+    if arr[0] == x:
+        return removeGivenElement(arr[1:],x)
+
+    return arr[0] + removeGivenElement(arr[1:], x)
+
+if __name__ == '__main__':
+    print(removeGivenElement('axbcxdxgxp','x'))
+    
