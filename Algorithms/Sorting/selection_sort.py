5a903280f6c9eab2f5bd5364b5774151b5ab6a50
suman gole <gole35830@gmail.com>
2023-01-22 09:45:48 +0545

    add selection sort

---
+++ Algorithms/Sorting/selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+if __name__ == "__main__":
+    arr = [2, 3, 10, 7, 4, 11, 6]
+    print(selection_sort(arr))
