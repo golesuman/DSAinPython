793d735210aca9cbe34c1eb2d2b622514648085b
suman gole <gole35830@gmail.com>
2023-09-10 19:36:44 +0545

    refactor: remove one extra loop

--- Arrays/Easy Problems/valid_anagram.py
+++ Arrays/Easy Problems/valid_anagram.py
@@ -1,9 +1,8 @@
 def is_anagram(s, t):
     count_char, count_char_t = {}, {}
-    for char in s:
+    for char, char1 in zip(s, t):
         count_char[char] = count_char.get(char, 0) + 1
-    for char in t:
-        count_char_t[char] = count_char_t.get(char, 0) + 1
+        count_char_t[char1] = count_char_t.get(char1, 0) + 1
     
     for key in count_char:
         if count_char[key] != count_char_t[key]:
