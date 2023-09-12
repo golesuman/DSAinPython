cf6c8b21c11c056a6610990659ab2e5b11b80668
suman gole <gole35830@gmail.com>
2023-09-12 20:34:59 +0545

    feat: valid sudoku

---
+++ Arrays/Medium Problems/valid_sudoku.py
@@ -0,0 +1,23 @@
+import collections
+
+
+def valid_sudoku(board):
+    rows = collections.defaultdict(set)
+    columns = collections.defaultdict(set)
+    squares = collections.defaultdict(set)
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if (
+                board[i][j] in rows[i]
+                or board[i][j] in columns[j]
+                or board[i][j] in squares[(i // 3, j // 3)]
+            ):
+                return False
+        rows[i].add(board[i][j])
+        columns[j].add(board[i][j])
+        squares[(i // 3, j // 3)].add(board[i][j])
+    return True
+
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(valid_sudoku(arr))
