8aa00fe11aff0486c4e08930caff36cd9d8a3b42
suman gole <gole35830@gmail.com>
2023-09-16 10:58:07 +0545

    feat: support for all numbers

--- Arrays/Medium Problems/TwoPointers/two_sum_if_input_arr_is_sorted.py
+++ Arrays/Medium Problems/TwoPointers/two_sum_if_input_arr_is_sorted.py
@@ -2,15 +2,18 @@ def two_sum_array_is_sorted(arr, target):
     left = 0
     right = len(arr) - 1
     while left <= right:
-        if arr[left] + arr[right] == target:
-            return left, right
-        left += 1
-        right -= 1
+        total = arr[left] + arr[right]
+        if total == target:
+            return [left, right]
+        if total > target:
+            right -= 1
+        else:
+            left += 1
 
 
 if __name__ == "__main__":
-    arr = [-1, 0]
-    target = -1
+    arr = [2, 7, 12, 15, 19, 20, 23]
+    target = 38
 
     result = two_sum_array_is_sorted(arr, target)
     print(result)
